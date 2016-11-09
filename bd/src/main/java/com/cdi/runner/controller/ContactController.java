8da4d0af4dee78d2791eef1eb9f664de1df3f45e
532051 <532051@PC310867.cts.com>
2016-11-09 17:32:31 +0530

    controller logic

--- bd/src/main/java/com/cdi/runner/controller/ContactController.java
+++ bd/src/main/java/com/cdi/runner/controller/ContactController.java
@@ -1,15 +1,21 @@
 package com.cdi.runner.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cdi.runner.JobExecutor;
+import com.cdi.runner.form.HiveSourceForm;
 import com.cdi.runner.form.JobForm;
+import com.cdi.runner.form.SourceForm;
+import com.cdi.runner.service.JobExecutorService;
 
 @Controller
 public class ContactController {
@@ -39,5 +45,27 @@ public class ContactController {
 		JobExecutor jb=	new JobExecutor(); 
 		String str =jb.executeJob(jf);
 		return "file_upload_success";
+	}
+	
+	
+	
+	@RequestMapping(value ="/savesource", method = RequestMethod.POST)
+    public ModelAndView processRegistration(@ModelAttribute("sourceForm") SourceForm sourceForm,
+            Map<String, Object> model) {
+		System.out.println("saving resource");
+		
+		JobExecutorService js = new JobExecutorService();
+		try {
+			js.writeFileToServer(sourceForm);
+		} catch (IllegalAccessException e) {
+			System.out.println("Error in creating file ");
+			e.printStackTrace();
+		}
+		ModelAndView view = new ModelAndView("success");
+		view.addObject("msg", "source configuration created successfully!");
+		System.out.println("saving resource complete");
+		return view;
+			
+		
 	}
 }
